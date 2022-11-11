1951b3443d72f991259263df9a91872350d35c56
Diggo07 <rodriggobr007@gmail.com>
2022-11-11 16:42:11 -0300

    Atributos do Usuario atualizados

--- mercearia-seu-joao.Model/UsuarioBD.cs
+++ mercearia-seu-joao.Model/UsuarioBD.cs
@@ -8,7 +8,12 @@ namespace mercearia_seu_joao.Model
 {
     public class UsuarioBD
     {
+        public int id { get; set; }
+        public int nome { get; set; }
         public int email { get; set; }
         public int senha { get; set; }
+        public int tipoUsuario { get; set; }
+        public int dataHoraAlteracao { get; set; }
+        public int dataHoraExclusao { get; set; }
     }
 }
